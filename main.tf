8089568489258f5b1ef7424f7d231e113388fcf0
riku1027 <45778163+riku1027@users.noreply.github.com>
2023-05-25 23:31:59 +0900

    terraform init

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  profile = "terraform-private"
+  region  = "ap-northeast-1"
+}
+
+resource "aws_instance" "hello-world" {
+  ami           = "ami-07c2a88388bb80eb0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-sandbox"
+  }
+  user_data = <<EOF
+#!/bin/bash
+amazon-linux-extras install -y nginx1.12
+systemctl start nginx
+EOF
+}
