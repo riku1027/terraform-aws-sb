f4c42c99e5e0119f1d29cd47cb15e9ceb110ef62
riku1027 <45778163+riku1027@users.noreply.github.com>
2023-05-28 14:03:22 +0900

    add operation management security group

--- security_group.tf
+++ security_group.tf
@@ -92,6 +92,55 @@ resource "aws_security_group_rule" "app_out_tcp3306" {
 }
 
 #-------------------------------------------
+# opmng security group
+#-------------------------------------------
+resource "aws_security_group" "opmng_sg" {
+  name        = "${var.project}-${var.environment}-opmng-sg"
+  description = "opmng role security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-opmng-sg"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_security_group_rule" "opmng_in_ssh" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "opmng_in_tcp3000" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "opmng_out_http" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+resource "aws_security_group_rule" "opmng_out_https" {
+  security_group_id = aws_security_group.opmng_sg.id
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+#-------------------------------------------
 # db security group
 #-------------------------------------------
 resource "aws_security_group" "db_sg" {
